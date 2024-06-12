a6a375cd95fbce5f57f575961bf8b136109dc70f
LiranBratt2121 <brattlirannin@gmail.com>
2024-06-12 06:10:34 +0300

    Add draw functions

--- include/window/window.h
+++ include/window/window.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <wtypes.h>
+#include "arm/arm.h"
 
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
@@ -8,9 +9,12 @@ class Window {
     public:
         Window();
         ~Window();
+        void Draw(Arm &arm);
         bool ProcessMessages();
     private:
         HINSTANCE m_hInstance;
         HWND m_hWnd;
         const wchar_t *CLASS_NAME = L"WindowClass";
+        Vector2d m_middlePoint;
+        Vector2d m_line1, m_line2;
 };
