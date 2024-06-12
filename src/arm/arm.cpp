c8f4a0c41e40473f2567c70571b6aea0b4f25f1f
LiranBratt2121 <brattlirannin@gmail.com>
2024-06-12 06:15:37 +0300

    Add update function vector math

--- src/arm/arm.cpp
+++ src/arm/arm.cpp
@@ -1,3 +1,31 @@
+#include "arm/arm.h"
+
+Arm::Arm(float len1, float len2, Vector2d &desiredPosition) : m_len1(len1), m_len2(len2), m_desiredPosition(desiredPosition){}
+
+void Arm::Update(){
+    m_pivot1Deg = GetBeta();
+    m_pivot2Deg = GetTheta();
+
+    float m_part1X = m_len1 * cos(m_pivot1Deg * TO_RADS);
+    float m_part1Y = m_len1 * sin(m_pivot1Deg * TO_RADS);
+
+    float m_part2X = m_len2 * cos(m_pivot2Deg * TO_RADS);
+    float m_part2Y = m_len2 * sin(m_pivot2Deg * TO_RADS);
+
+    m_part1.Update(m_part1X, m_part1Y);
+    m_part2.Update(m_part2X, m_part2Y);
+}
+
+Vector2d& Arm::GetPart1()
+{
+    return m_part1;
+}
+
+Vector2d& Arm::GetPart2()
+{
+    return m_part2;
+}
+
 float Arm::GetAlpha(){
     float numerator = (m_len1 * m_len1) + (m_len2 * m_len2) - pow(GetDesiredPosVectorDistance(), 2);
     float denominator = 2 * m_len1 * m_len2;
