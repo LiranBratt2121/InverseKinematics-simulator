aa57c6b22df103dc9502ea4fb6a0ffa6f783ed0e
LiranBratt2121 <brattlirannin@gmail.com>
2024-06-13 20:34:09 +0300

    Reformat access modifiers

--- include/window/window.h
+++ include/window/window.h
@@ -9,16 +9,19 @@ class Window {
     public:
         Window();
         ~Window();
-        void Draw();
-        void DrawGrid() const;
+        void DrawArm(HDC& hdc) const;
+        void DrawGrid(HDC& hdc) const;
         bool ProcessMessages();
-        static Vector2d GetScreenXY(LPARAM lParam);
+        Vector2d GetScreenXY(LPARAM lParam) const;
+        void UpdateOnScreenVectors(LPARAM lParam);
+
     private:
         HINSTANCE m_hInstance;
         HWND m_hWnd;
         const wchar_t *CLASS_NAME = L"WindowClass";
+        Arm m_arm;
         Vector2d m_middlePoint;
         Vector2d m_line1, m_line2;
-    private:
-        void DrawLine(const HDC& hdc, const Vector2d &start, const Vector2d &end) const;
+        
+        void DrawLine(HDC hdc, const Vector2d& start, const Vector2d& end) const;
 };
