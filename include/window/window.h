4750aa01e197d63576362bdc109b6a069ee57392
LiranBratt2121 <brattlirannin@gmail.com>
2024-06-13 18:30:44 +0300

    Add draw grid declaration

--- include/window/window.h
+++ include/window/window.h
@@ -9,14 +9,16 @@ class Window {
     public:
         Window();
         ~Window();
-        void Draw(Arm &arm);
+        void Draw();
+        void DrawGrid() const;
         bool ProcessMessages();
         static Vector2d GetScreenXY(LPARAM lParam);
-        void DrawGrid();
     private:
         HINSTANCE m_hInstance;
         HWND m_hWnd;
         const wchar_t *CLASS_NAME = L"WindowClass";
         Vector2d m_middlePoint;
         Vector2d m_line1, m_line2;
+    private:
+        void DrawLine(const HDC& hdc, const Vector2d &start, const Vector2d &end) const;
 };
