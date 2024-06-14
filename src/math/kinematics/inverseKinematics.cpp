1d3b9b8a8931bce43e799a26be9d916f4eddcf0c
LiranBratt2121 <brattlirannin@gmail.com>
2024-06-14 08:28:25 +0300

    Impl all helper function for the calculations

---
+++ src/math/kinematics/inverseKinematics.cpp
@@ -0,0 +1,28 @@
+#include "kinematics/inverseKinematics.h"
+
+InverseKinematics::InverseKinematics(const float len1, const float len2, Vector2d& desiredPosition)
+    : m_desiredPosition(desiredPosition), m_len1(len1), m_len2(len2) {}
+
+float InverseKinematics::GetAlpha() {
+    float numerator = (m_len1 * m_len1) + (m_len2 * m_len2) - pow(GetDesiredPosVectorDistance(), 2);
+    float denominator = 2 * m_len1 * m_len2;
+    return acos(numerator / denominator) * TO_DEGREES;
+}
+
+float InverseKinematics::GetBeta() {
+    return GetLambda() + GetTheta();
+}
+
+float InverseKinematics::GetDesiredPosVectorDistance() {
+    return m_desiredPosition.GetLength();
+}
+
+float InverseKinematics::GetLambda() {
+    return 90 - m_desiredPosition.GetArgument();
+}
+
+float InverseKinematics::GetTheta() {
+    float numerator = m_len2 * sin(GetAlpha() * TO_RADS);
+    float denominator = GetDesiredPosVectorDistance();
+    return asin(numerator / denominator) * TO_DEGREES;
+}
