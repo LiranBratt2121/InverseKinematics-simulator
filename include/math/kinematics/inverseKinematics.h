4fb05187e62593aa39c4fe2effd39c9783a6b39b
LiranBratt2121 <brattlirannin@gmail.com>
2024-06-14 08:27:42 +0300

    Add InverseKinematics class header

---
+++ include/math/kinematics/inverseKinematics.h
@@ -0,0 +1,16 @@
+#include "math/vector/vector2d.h"
+
+class InverseKinematics {
+    public:
+        InverseKinematics(const float len1, const float len2, Vector2d& desiredPosition);
+        void Solve(float& pivotAngle1, float& pivotAngle2);
+    private:
+        float GetAlpha();
+        float GetBeta();
+        float GetDesiredPosVectorDistance();
+        float GetLambda();
+        float GetTheta();
+
+        Vector2d& m_desiredPosition;
+        const float m_len1, m_len2;
+};
