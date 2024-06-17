51b271ac30982aaf634679b519b3772d54938900
LiranBratt2121 <brattlirannin@gmail.com>
2024-06-17 12:36:32 +0300

    Make constructor accecpt rvalues

--- src/arm/arm.cpp
+++ src/arm/arm.cpp
@@ -1,7 +1,7 @@
 #include "arm/arm.h"
 
-Arm::Arm(const float len1, const float len2, const Vector2d &desiredPosition) 
-    : m_len1(len1), m_len2(len2), m_desiredPosition(desiredPosition), m_inverseKinematics(len1, m_len2, desiredPosition) {}
+Arm::Arm(const float len1, const float len2, Vector2d&& desiredPosition) 
+    : m_len1(len1), m_len2(len2), m_desiredPosition(desiredPosition), m_inverseKinematics(len1, len2, desiredPosition) {}
 
 void Arm::Update() {
     m_inverseKinematics.Solve(m_pivot1Deg, m_pivot2Deg);
